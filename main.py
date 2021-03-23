ab04d27f4b811d55affc8853810b0086d93c13c1
iyasha <mr.robot0n@gmail.com>
2021-03-23 19:42:00 +0200

    init

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-import logging
-import logging.config
+from flask import Flask, render_template
 
-logging.config.fileConfig('logger.conf')
-logger = logging.getLogger('main')
+app = Flask(__name__)
 
 
-class Auto:
+@app.route('/')
+def main_page():
+    return render_template('index.html', title='ItStep Blog')
 
-    def __init__(self, mileage: int, brand: str, release_year: int):
-        logger.debug(f'{mileage} | {brand} | {release_year}')
-        self.mileage = mileage
-        self.brand = brand
-        self.release_year = release_year
 
-    def car_years(self) -> int:
-        try:
-            return 2021 - self.release_year
-        except Exception as e:
-            logger.error(e)
-
-    def format_brand(self) -> str:
-        logger.info('Пользователь вызвал функцию format_brand')
-        return f'Auto: {self.brand}'
+@app.route('/about')
+def about_page():
+    return 'About page!'
 
 
 if __name__ == '__main__':
-    cars = [Auto(12314, 'BMW', 2004), Auto(124124, 'Lada', 2013), Auto(123124, 'asdasdasd', '2019')]
-
-    for car in cars:
-        print(f'{car.format_brand()} | {car.car_years()}')
+    app.run(host='localhost', port=5000)
